4192dc66387a74e58e0ddc1c8fbc484c85b3e421
src_resources <src.res.211@gmail.com>
2023-10-21 13:13:29 +0800

    v0.1.1

--- tyut_plugin/constants.py
+++ tyut_plugin/constants.py
@@ -7,7 +7,7 @@ from tyut_plugin.module.dustman.module import DustmanModule
 from tyut_plugin.module.maintenance_notice.module import MaintenanceNoticeModule
 
 PLUGIN_ID = 'tyut_plugin'
-PLUGIN_VERSION = '0.1.0'
+PLUGIN_VERSION = '0.1.1'
 PLUGIN_COMMAND_PREFIX = '!!tyut'
 
 MODULES = [CoreModule, TimeAnnouncementModule, ScheduledRestartModule, MaintenanceNoticeModule]

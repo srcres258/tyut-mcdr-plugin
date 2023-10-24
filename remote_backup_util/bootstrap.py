d5580137ce99be530ce806a64aa48d43b9eb80d6
src_resources <src.res.211@gmail.com>
2023-10-24 22:03:56 +0800

    Add infinite loop within bootstrap
    As needed personally, repeat the logic to avoid launching this python
    program again and again.
    
    Signed-off-by: src_resources <src.res.211@gmail.com>

--- remote_backup_util/bootstrap.py
+++ remote_backup_util/bootstrap.py
@@ -2,10 +2,11 @@
 from remote_backup_util.app import server, client
 
 def bootstrap(launch_server: bool = True):
-    if launch_server:
-        server.main()
-    else:
-        client.main()
+    while True:
+        if launch_server:
+            server.main()
+        else:
+            client.main()
 
 def main():
     char = input("To launch the client enter '0', otherwise the server will be launched:")
